7863b7562ff45ca30598f9777c984cf2301de265
Francisco Piñero <frannpinero@hotmail.com>
2024-09-16 20:45:07 -0300

    Agregar archivos de proyecto.

---
+++ MemDinamicaTablaHash/Ejercicio78.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace MemDinamicaTablaHash
+{
+    internal class Ejercicio78
+    {
+        static void Main()
+        {
+            Hashtable  diccio = new Hashtable();
+            diccio.Add("Hello", "Hola.");
+            diccio.Add("My name is Fran","Me llamo Fran,");
+            diccio.Add("How is your name","¿Cómo te llamas?");
+
+            string texto = "Hola. Me llamo Fran, ¿Cómo te llamas?";
+            string[] partes = texto.Split(',');
+
+            foreach (string parte in partes)
+            {
+                Console.WriteLine(parte);
+            }
+
+            if (diccio.Contains("adiós"))
+                Console.WriteLine(diccio["adiós"]);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MemDinamicaTablaHash/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("MemDinamicaTablaHash")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MemDinamicaTablaHash")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("996f54e3-aa4e-4d61-b935-94e8cc29d429")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
